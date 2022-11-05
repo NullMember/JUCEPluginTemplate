0206598da1c6e8af32ebda8afd3ed196e66b5522
Malik Enes Safak <e.maliksafak@gmail.com>
2022-11-05 12:20:17 +0300

    Parameter names

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -8,7 +8,7 @@ AudioPlugin::AudioPlugin()
     addParameter (gain = new AudioParameterFloat ({ "gain", 1 }, "Gain", 0.0f, 1.0f, 0.5f));
 }
 
-void AudioPlugin::prepareToPlay (double, int){
+void AudioPlugin::prepareToPlay (double sampleRate, int maximumExpectedSamplesPerBlock){
 
 }
 
@@ -16,24 +16,26 @@ void AudioPlugin::releaseResources(){
 
 }
 
-void AudioPlugin::processBlock (AudioBuffer<float>& buffer, MidiBuffer&)
+void AudioPlugin::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiBuffer)
 {
     buffer.applyGain (*gain);
 }
 
-void AudioPlugin::processBlock (AudioBuffer<double>& buffer, MidiBuffer&)
+void AudioPlugin::processBlock (AudioBuffer<double>& buffer, MidiBuffer& midiBuffer)
 {
     buffer.applyGain ((float) *gain);
 }
 
 void AudioPlugin::getStateInformation (MemoryBlock& destData)
 {
-    MemoryOutputStream (destData, true).writeFloat (*gain);
+    auto memoryOutputStream = MemoryOutputStream(destData, true);
+    memoryOutputStream.writeFloat(*gain);
 }
 
 void AudioPlugin::setStateInformation (const void* data, int sizeInBytes)
 {
-    gain->setValueNotifyingHost (MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readFloat());
+    auto memoryInputStream = MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false);
+    gain->setValueNotifyingHost(memoryInputStream.readFloat());
 }
 
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
