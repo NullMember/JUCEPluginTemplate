8a1b8668e962d2090b1488e31d992a0745ac06ff
Malik Enes Safak <e.maliksafak@gmail.com>
2022-11-05 15:21:22 +0300

    Use processSamples in processBlock functions

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -2,50 +2,53 @@
 #include "Plugin.h"
 
 AudioPlugin::AudioPlugin()
-    : AudioProcessor (BusesProperties().withInput  ("Input",  AudioChannelSet::stereo())
-                                        .withOutput ("Output", AudioChannelSet::stereo()))
+    : AudioProcessor(BusesProperties().withInput("Input", AudioChannelSet::stereo())
+                                      .withOutput("Output", AudioChannelSet::stereo()))
 {
-    addParameter (gain = new AudioParameterFloat ({ "gain", 1 }, "Gain", 0.0f, 1.0f, 0.5f));
+    addParameter(gain = new AudioParameterFloat({"gain", 1}, "Gain", 0.0f, 1.0f, 0.5f));
 }
 
-void AudioPlugin::prepareToPlay (double sampleRate, int maximumExpectedSamplesPerBlock){
-
-}
-
-void AudioPlugin::releaseResources(){
+void AudioPlugin::prepareToPlay(double sampleRate, int maximumExpectedSamplesPerBlock)
+{
 
 }
 
-template<typename T>
-void AudioPlugin::processSamples(AudioBuffer<T>& buffer, MidiBuffer& midiBuffer){
+void AudioPlugin::releaseResources()
+{
+
+}
+
+template <typename T>
+void AudioPlugin::processSamples(AudioBuffer<T> &buffer, MidiBuffer &midiBuffer)
+{
     buffer.applyGain(*gain);
 }
 
-void AudioPlugin::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiBuffer)
+void AudioPlugin::processBlock(AudioBuffer<float> &buffer, MidiBuffer &midiBuffer)
 {
-    buffer.applyGain (*gain);
+    processSamples<float>(buffer, midiBuffer);
 }
 
-void AudioPlugin::processBlock (AudioBuffer<double>& buffer, MidiBuffer& midiBuffer)
+void AudioPlugin::processBlock(AudioBuffer<double> &buffer, MidiBuffer &midiBuffer)
 {
-    buffer.applyGain ((float) *gain);
+    processSamples<double>(buffer, midiBuffer);
 }
 
-void AudioPlugin::getStateInformation (MemoryBlock& destData)
+void AudioPlugin::getStateInformation(MemoryBlock &destData)
 {
-    MemoryOutputStream* memoryOutputStream = new MemoryOutputStream(destData, true);
+    MemoryOutputStream *memoryOutputStream = new MemoryOutputStream(destData, true);
     memoryOutputStream->writeFloat(*gain);
     delete memoryOutputStream;
 }
 
-void AudioPlugin::setStateInformation (const void* data, int sizeInBytes)
+void AudioPlugin::setStateInformation(const void *data, int sizeInBytes)
 {
-    MemoryInputStream* memoryInputStream = new MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false);
+    MemoryInputStream *memoryInputStream = new MemoryInputStream(data, static_cast<size_t>(sizeInBytes), false);
     gain->setValueNotifyingHost(memoryInputStream->readFloat());
     delete memoryInputStream;
 }
 
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter()
 {
     return new AudioPlugin();
 }
